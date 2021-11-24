fee7cace004a020f254ceaddb3ce132968bae682
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-11-24 12:52:44 -0600

    game now fully functional

--- src/index.js
+++ src/index.js
@@ -57,10 +57,17 @@ class Board extends React.Component {
   // var newPlayer = {...player, score: 2};
 
   handleClick(i) {
+    //changed the Board’s handleClick function to return early by ignoring a click if someone has won the game or if a Square is already filled:
     const squares = this.state.squares.slice();
-    // updated the Board’s handleClick function to flip the value of xIsNext:
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
     squares[i] = this.state.xIsNext ? "X" : "O";
-    this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -74,7 +81,15 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    //We will call calculateWinner(squares) in the Board’s render function to check if a player has won.
+    // If a player has won, we can display text such as “Winner: X” or “Winner: O”.
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
 
     return (
       <div>
@@ -113,6 +128,29 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+//Helper Function: calculateWinner.
+
+//Given an array of 9 squares, this function will check for a winner and return 'X', 'O', or null as appropriate.
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
