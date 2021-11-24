3a1dd0528e893d0f1423d463f9839d29f560f798
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-11-24 10:21:16 -0600

    prop being passed from the renderSquare method in the Board component to the Square componenent and added pseudo code

--- src/index.js
+++ src/index.js
@@ -2,15 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+// By inspecting the code, you’ll notice that we have three React components: Square, Board and Game.
+// The Square Component renders a single <button> and the Board renders 9 squares.
+// The Game component renders a board with placeholder values
+
+// Here Square is a React component CLASS or React Component Type.  A component takes in parameters called PROPS (short for 'properties'), and returns
+// a hierchy of views to display via the render() method.  We use these components to tell React what we want to see on the screen.  When our
+// data changes React will efficiently update and re-render our components.
 class Square extends React.Component {
+  // the render method returns a description of what you want to see on the screen.  React takes the description and displays the result.
+  // In particular render return a REACT ELEMENT, which is a lightweight description of what to render.
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    // showing the value being passed from the value prop from the renderSquare method.
+    return <button className="square">{this.props.value}</button>;
   }
 }
 
 class Board extends React.Component {
+  //passing a PROP called value which is passing data from our Board component to our Square component.
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
